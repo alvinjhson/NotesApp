ac226f06e269d6d5ab22211cc0bda9a0ec791955
alvinjhson <alvinbest26@gmail.com>
2024-04-18 15:03:18 +0200

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by Alvin Johansson on 2024-04-18.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
